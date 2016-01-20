b1593c2c49beb656da5d729b3ef9366ad1c7b9b3
Jade Benjamin-Chung <jadebc@gmail.com>
2016-01-20 09:40:40 -0800

    Updates to files creating tables for other analyses

---
+++ src/Tables/other/4-table-supp-CIR-beach.R
@@ -0,0 +1,116 @@
+##########################################
+# Coliphage analysis - 6 beaches
+# v1 by Jade 7/13/15
+
+# This file makes a table with regression output
+# For a table with the combined and interaction
+# results for body immersion
+##########################################
+
+rm(list=ls())
+
+load("~/Documents/CRG/coliphage/results/rawoutput/regress-10day-body-beach.Rdata")
+
+# ------------------------------------------------
+# combine output
+# ------------------------------------------------
+# n's
+gici10.n.comb=c(do.n10.fmc1601, mb.n10.fmc1601,
+				av.n10.fmc1602, do.n10.fmc1602,
+				av.n10.fpc1601, do.n10.fpc1601, fa.n10.fpc1601, ma.n10.fpc1601, mb.n10.fpc1601,
+        av.n10.fpc1602, do.n10.fpc1602)
+
+gici10.n.int0=c(av.n10.fmc1602.int0, do.n10.fmc1602.int0,
+        av.n10.fpc1601.int0, do.n10.fpc1601.int0,  ma.n10.fpc1601.int0, 
+        av.n10.fpc1602.int0, do.n10.fpc1602.int0)
+
+gici10.n.int1=c(av.n10.fmc1602.int1, do.n10.fmc1602.int1,
+        av.n10.fpc1601.int1, do.n10.fpc1601.int1, ma.n10.fpc1601.int1,
+        av.n10.fpc1602.int1, do.n10.fpc1602.int1)
+
+# estimates pooled across conditions
+gici10.comb=list(dofit10.fmc1601, mbfit10.fmc1601,
+        avfit10.fmc1602, dofit10.fmc1602,
+        avfit10.fpc1601, dofit10.fpc1601, fafit10.fpc1601, mafit10.fpc1601, mbfit10.fpc1601,
+                avfit10.fpc1602, dofit10.fpc1602)
+
+# estimates when berm open/ groundwater above median
+gici10.int1=list(avfit10.fmc1602.gw.1, dofit10.fmc1602.berm.1,avfit10.fpc1601.gw.1,
+        dofit10.fpc1601.berm.1,avfit10.fpc1602.gw.1,  dofit10.fpc1602.berm.1,mafit10.fpc1601.berm.1)
+
+# estimates when berm closed / groundwater below median
+gici10.int0=list(avfit10.fmc1602.gw.0,dofit10.fmc1602.berm.0, avfit10.fpc1601.gw.0, 
+        dofit10.fpc1601.berm.0, avfit10.fpc1602.gw.0, dofit10.fpc1602.berm.0,mafit10.fpc1601.berm.0)
+
+# ------------------------------------------------
+# function to make table row with exponentiated point estimate
+# and 95% ci in parentheses
+# ------------------------------------------------
+mkrow=function(out){
+  pt.est=out$fit[2,1]
+  lb=pt.est-qnorm(.975)*out$fit[2,2]
+  ub=pt.est+qnorm(.975)*out$fit[2,2]
+  paste(sprintf("%0.2f",exp(pt.est))," (",
+        sprintf("%0.2f",exp(lb)),",",
+        sprintf("%0.2f",exp(ub)), ")",sep="")
+}
+
+# ------------------------------------------------
+# convert results into table format
+# ------------------------------------------------
+
+# 10-day gici ----------------------------------------------------
+gici10.tab.comb=data.frame(combined=unlist(lapply(gici10.comb,mkrow)))
+gici10.tab.comb$combined=as.character(gici10.tab.comb$combined)
+
+# stratify by berm/groundwater
+gici10.tab.int1=data.frame(int1=unlist(lapply(gici10.int1,mkrow)))
+gici10.tab.int0=data.frame(int0=unlist(lapply(gici10.int0,mkrow)))
+gici10.tab.int1$int1=as.character(gici10.tab.int1$int1)
+gici10.tab.int0$int0=as.character(gici10.tab.int0$int0)
+
+# combine the pooled and high/low risk estimates into one object
+gici10.tab.all=c(gici10.tab.comb$combined[1:3],gici10.tab.int0$int0[1],gici10.tab.int1$int1[1],
+                gici10.tab.comb$combined[4],gici10.tab.int0$int0[2],gici10.tab.int1$int1[2],
+                gici10.tab.comb$combined[5],gici10.tab.int0$int0[3],gici10.tab.int1$int1[3],
+                gici10.tab.comb$combined[6],gici10.tab.int0$int0[4],gici10.tab.int1$int1[4],
+                gici10.tab.comb$combined[7:8],gici10.tab.int0$int0[5],gici10.tab.int1$int1[5],
+                gici10.tab.comb$combined[9:10],gici10.tab.int0$int0[6],gici10.tab.int1$int1[6],
+                gici10.tab.comb$combined[11],gici10.tab.int0$int0[7],gici10.tab.int1$int1[7])
+
+gici10.n.all=c(gici10.n.comb[1:3],gici10.n.int0[1],gici10.n.int1[1],
+                gici10.n.comb[4],gici10.n.int0[2],gici10.n.int1[2],
+                gici10.n.comb[5],gici10.n.int0[3],gici10.n.int1[3],
+                gici10.n.comb[6],gici10.n.int0[4],gici10.n.int1[4],
+                gici10.n.comb[7:8],gici10.n.int0[5],gici10.n.int1[5],
+                gici10.n.comb[9:10],gici10.n.int0[6],gici10.n.int1[6],
+                gici10.n.comb[11],gici10.n.int0[7],gici10.n.int1[7])
+
+
+label=c("Doheny","Mission Bay",
+        "Avalon","","","Doheny","","",
+        "Avalon","","","Doheny","","","Fairhope","Malibu","","","Mission Bay",
+        "Avalon","","","Doheny","","")
+
+gici10.comb.print=data.frame(cbind(label,gici10.n.all,gici10.tab.all))
+gici10.comb.print$label=as.character(gici10.comb.print$label)
+
+gici10.comb.print$cond=c(rep("All conditions",3),"Groundwater below median flow",
+      "Groundwater above median flow","All conditions","Berm closed","Berm open",
+      "All conditions","Groundwater below median flow","Groundwater above median flow",
+      "All conditions","Berm closed","Berm open","All conditions","All conditions",
+      "Berm closed","Berm open","All conditions","All conditions","Groundwater below median flow",
+      "Groundwater above median flow","All conditions","Berm closed","Berm open")
+
+gici10.comb.print=gici10.comb.print[,c(1,4,2:3)]
+
+colnames(gici10.comb.print)=c("label","cond","N","CIR")
+
+save(gici10.comb.print,
+     file="~/Documents/CRG/coliphage/Results/Tables/reg-combined.RData")
+
+
+
+
+
+
---
+++ src/Tables/other/8-table-continuous-negcontrol.R
@@ -0,0 +1,134 @@
+##########################################
+# Coliphage analysis - 6 beaches
+# v1 by Jade 7/13/15
+
+# Comparison of CIRs for continuous 
+# coliphage indicators for main analysis
+# and negative controls
+
+##########################################
+
+
+rm(list=ls())
+load("~/Documents/CRG/coliphage/results/rawoutput/regress-10day-continuous-body.Rdata")
+load("~/Documents/CRG/coliphage/results/rawoutput/regress-10day-continuous-body-negcontrol.Rdata")
+load("~/Documents/CRG/coliphage/results/rawoutput/regress-10day-continuous-body-entero.Rdata")
+load("~/Documents/CRG/coliphage/results/rawoutput/regress-10day-continuous-body-entero-negcontrol.Rdata")
+
+
+
+     
+# --------------------------------------
+# formatting function for CIR results
+# --------------------------------------
+CIRformat <- function(x) {
+  # x : vector of length 3 with CIR, CIRlb, Cub
+  paste(sprintf("%1.2f",x[1])," (",sprintf("%1.2f",x[2]),", ",sprintf("%1.2f",x[3]),")",sep="")
+} 
+
+# --------------------------------------
+# get CIs in one object for graphing dose
+# response figures
+# --------------------------------------
+# function to get CIR Estimates and CIs from simple stratified models
+getCIR <- function(x) {
+  # x : log-linear model object returned from coeftest (class=coeftest)
+  # NOTE: assumes exposure of interest is the first covariate and there are no interactions
+  est <- exp(x[2,1])
+  se  <- x[2,2]  
+  lb <- exp(log(est)-1.96*se)
+  ub <- exp(log(est)+1.96*se)
+  res <- c(est,lb,ub)
+  return(res)
+}
+
+# --------------------------------------
+# compile estimates
+# --------------------------------------
+
+# coliphage main 
+all=list(overall.fit10.fmc1601, overall.fit10.fmc1602, 
+  overall.fit10.fpc1601,overall.fit10.fpc1602)
+
+low=list(overall.fit10.fmc1602.low,overall.fit10.fpc1601.low,overall.fit10.fpc1602.low)
+
+high=list(overall.fit10.fmc1602.high,overall.fit10.fpc1601.high,overall.fit10.fpc1602.high)
+
+# entero main
+all.entero=list(overall.fit10.entero.fmc1601,overall.fit10.entero.fmc1602,
+  overall.fit10.entero.fpc1601,overall.fit10.entero.fpc1602)
+
+high.entero=list(overall.fit10.entero.high.fmc1602,overall.fit10.entero.high.fpc1601,
+  overall.fit10.entero.high.fpc1602)
+
+low.entero=list(overall.fit10.entero.low.fmc1602,overall.fit10.entero.low.fpc1601,
+  overall.fit10.entero.low.fpc1602)
+
+# coliphage negative control
+nc.all=list(nc.overall.fit10.fmc1601, nc.overall.fit10.fmc1602, 
+  nc.overall.fit10.fpc1601,nc.overall.fit10.fpc1602)
+
+nc.low=list(nc.overall.fit10.fmc1602.low,nc.overall.fit10.fpc1601.low,nc.overall.fit10.fpc1602.low)
+
+nc.high=list(nc.overall.fit10.fmc1602.high,nc.overall.fit10.fpc1601.high,nc.overall.fit10.fpc1602.high)
+
+# entero negative control
+nc.all.entero=list(nc.overall.fit10.entero.fmc1601,nc.overall.fit10.entero.fmc1602,
+  nc.overall.fit10.entero.fpc1601,nc.overall.fit10.entero.fpc1602)
+
+nc.high.entero=list(nc.overall.fit10.entero.high.fmc1602,nc.overall.fit10.entero.high.fpc1601,
+  nc.overall.fit10.entero.high.fpc1602)
+
+nc.low.entero=list(nc.overall.fit10.entero.low.fmc1602,nc.overall.fit10.entero.low.fpc1601,
+  nc.overall.fit10.entero.low.fpc1602)
+
+# --------------------------------------
+# format estimates
+# --------------------------------------
+est.all=unlist(lapply(all,function(x) CIRformat(getCIR(x))))
+est.low=c(NA,unlist(lapply(low,function(x) CIRformat(getCIR(x)))))
+est.high=c(NA,unlist(lapply(high,function(x) CIRformat(getCIR(x)))))
+
+est.all.entero=unlist(lapply(all.entero,function(x) CIRformat(getCIR(x))))
+est.low.entero=c(NA,unlist(lapply(low.entero,function(x) CIRformat(getCIR(x)))))
+est.high.entero=c(NA,unlist(lapply(high.entero,function(x) CIRformat(getCIR(x)))))
+
+est.nc.all=unlist(lapply(nc.all,function(x) CIRformat(getCIR(x))))
+est.nc.low=c(NA,unlist(lapply(nc.low,function(x) CIRformat(getCIR(x)))))
+est.nc.high=c(NA,unlist(lapply(nc.high,function(x) CIRformat(getCIR(x)))))
+
+est.nc.all.entero=unlist(lapply(nc.all.entero,function(x) CIRformat(getCIR(x))))
+est.nc.low.entero=c(NA,unlist(lapply(nc.low.entero,function(x) CIRformat(getCIR(x)))))
+est.nc.high.entero=c(NA,unlist(lapply(nc.high.entero,function(x) CIRformat(getCIR(x)))))
+
+
+# --------------------------------------
+# make table
+# --------------------------------------
+col.tab=cbind(est.all,est.low,est.high)
+col.tab.nc=cbind(est.nc.all,est.nc.low,est.nc.high)
+
+ent.tab=cbind(est.all.entero,est.low.entero,est.high.entero)
+ent.tab.nc=cbind(est.nc.all.entero,est.nc.low.entero,est.nc.high.entero)
+
+colnames(col.tab)=c("all","low","high")
+colnames(col.tab.nc)=c("all","low","high")
+colnames(ent.tab)=c("all","low","high")
+colnames(ent.tab.nc)=c("all","low","high")
+
+cont.tab=rbind(col.tab[1,],ent.tab[1,],col.tab[2,],ent.tab[2,],
+               col.tab[3,],ent.tab[3,],col.tab[4,],ent.tab[4,])
+
+cont.tab.nc=rbind(col.tab.nc[1,],ent.tab.nc[1,],col.tab.nc[2,],ent.tab.nc[2,],
+               col.tab.nc[3,],ent.tab.nc[3,],col.tab.nc[4,],ent.tab.nc[4,])
+
+cont.tab.out=rbind(cont.tab,cont.tab.nc)
+
+label=rep(c("Coliphage","Enterococcus"),8)
+analysis=c(rep("Main",8),rep("Neg control",8))
+
+cont.tab.out=cbind(label,analysis,cont.tab.out)
+
+save(cont.tab.out,file="~/Documents/CRG/coliphage/results/tables/table-cont-negcontrol.RData")
+
+
